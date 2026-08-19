5ff8678f463a6462ca31174c05e8504b08af4e89
Ali Yağız Demir <aliyagiz207@gmail.com>
2026-08-19 22:39:02 +0300

    Add MvcFirmaCagri ASP.NET MVC project

---
+++ MvcFirmaCagri/MvcFirmaCagri/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using MvcFirmaCagri.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcFirmaCagri.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Dbis_TakipEntities db = new Dbis_TakipEntities();
+        public ActionResult Index()
+        {
+            //kodlar
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblFirmalar p)
+        {
+            var bilgiler = db.TblFirmalar.FirstOrDefault(x => x.Mail == p.Mail && x.Sifre == p.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Mail, false);
+                Session["Mail"] = bilgiler.Mail.ToString();
+                return RedirectToAction("AktifCagrilar", "Default");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
